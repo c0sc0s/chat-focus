2c692f53f7e58c0ae09009e5d9d7cceec602e1e1
wenjie.02 <wenjie.02@bytedance.com>
2025-04-30 13:44:47 +0800

    init3

---
+++ server/src/api/plugins/index.ts
@@ -0,0 +1,28 @@
+import dbPlugin from "./db";
+import authPlugin from "./auth";
+import websocketPlugin from "./websocket";
+import { FastifyInstance } from "fastify";
+import { publicPaths } from "../config";
+
+const AppPlugins = [
+  {
+    plugin: dbPlugin,
+  },
+  {
+    plugin: authPlugin,
+    options: {
+      publicPaths: publicPaths,
+    },
+  },
+  {
+    plugin: websocketPlugin,
+  },
+];
+
+const registerPlugins = (fastify: FastifyInstance) => {
+  AppPlugins.forEach((plugin) => {
+    fastify.register(plugin.plugin, plugin.options);
+  });
+};
+
+export default registerPlugins;
